6e76f01b28235dec2ff9b9f87317a301a768e6ae
Lucas Emanuel <lucasemanuel075@gmail.com>
2026-03-15 18:34:31 -0300

    cha-de-paela 0.3

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import IdentificacaoPage from "./pages/Identificacao/IdentificacaoPage";
-import IntroducaoPage from "./pages/Introducao/IntroducaoPage";
+import IdentificacaoPage from "./src/pages/Identificacao/IdentificacaoPage";
+import IntroducaoPage from "./src/pages/Introducao/IntroducaoPage";
 
 function App() {
   return (
